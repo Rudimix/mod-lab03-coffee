ff035c93019d74eed8921602fcf0f31cf23c1e12
Rudimix <61971808+Rudimix@users.noreply.github.com>
2023-03-18 17:14:35 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Automata.h"
+
+int main() {
+    Automata machine = Automata();
+    machine.on();
+    machine.coin(100);
+    machine.choice(1);
+    machine.cook();
+    machine.finish();
+    return 0;
+}
